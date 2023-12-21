7ce99a326aa34497bc9b6a69a12ab9d5543848ea
Caleb Eden <ceden0207@yahoo.com>
2023-12-21 16:11:11 -0600

    feat(end_to_end_test.py): add pytest to compile and run sample programs

---
+++ test/end_to_end_tests/end_to_end_test.py
@@ -0,0 +1,86 @@
+import os
+import pathlib
+import subprocess
+from typing import TextIO
+
+import pytest
+
+# Should be in test/ when you run pytest or use --rootdir=test
+canyon_compiler = os.path.join(os.getcwd(), "../build/canyon")
+os.chdir("end_to_end_tests")
+tests = os.path.join(os.getcwd(), "tests")
+
+
+def canyon(infile: str, outfile: str, stdin: TextIO, stdout: TextIO, stderr: TextIO):
+    return subprocess.Popen([canyon_compiler, infile, outfile], stdin=stdin, stdout=stdout, stderr=stderr)
+
+
+def gcc(infile: str, outfile: str, stdin: TextIO, stdout: TextIO, stderr: TextIO):
+    return subprocess.Popen(["gcc", infile, "-o", outfile], stdin=stdin, stdout=stdout, stderr=stderr)
+
+
+def run(program: str, stdin: TextIO, stdout: TextIO, stderr: TextIO):
+    return subprocess.Popen(program, stdin=stdin, stdout=stdout, stderr=stderr)
+
+
+def discover_tests(directory: str):
+    return [pathlib.Path(entry).name for entry in os.listdir(directory) if os.path.isdir(os.path.join(directory, entry))]
+
+
+@pytest.mark.parametrize("test_name", discover_tests(os.path.join(tests, "success")))
+def test_success(test_name, monkeypatch: pytest.MonkeyPatch, tmp_path: pathlib.Path):
+    source = os.path.join(tests, "success", test_name)
+    monkeypatch.chdir(tmp_path)
+    pathlib.Path("output").mkdir(exist_ok=True)
+
+    process = canyon(os.path.join(source, "main.canyon"), "main.c", None,
+                     subprocess.PIPE, subprocess.PIPE)
+    out, err = process.communicate()
+    assert process.wait() == 0
+    assert out.decode() == ""
+    # assert err.decode() == ""
+
+    process = gcc("main.c", "a.out", None, subprocess.PIPE, subprocess.PIPE)
+    assert process.wait() == 0
+    out, err = process.communicate()
+    assert process.wait() == 0
+    assert out.decode() == ""
+    assert err.decode() == ""
+
+    process = subprocess.Popen("./a.out", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    assert process.wait() == 0
+
+    exp_out_file = os.path.join(source, "expected/stdout")
+    exp_err_file = os.path.join(source, "expected/stderr")
+    try:
+        with open(exp_out_file, "r") as exp_out:
+            expected_stdout = exp_out.read()
+    except FileNotFoundError:
+        expected_stdout = ""
+    try:
+        assert out.decode() == expected_stdout
+    except AssertionError:
+        pathlib.Path(os.path.join(source, "failure")).mkdir(exist_ok=True)
+        diff = subprocess.Popen(
+            ["diff", "-N", exp_out_file, "-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        diff_out, diff_err = diff.communicate(input=out)
+        with open(os.path.join(source, "failure/stdout"), "w") as fail_out:
+            fail_out.write(diff_out.decode())
+        raise
+
+    try:
+        with open(exp_err_file, "r") as exp_err:
+            expected_stderr = exp_err.read()
+    except FileNotFoundError:
+        expected_stderr = ""
+    try:
+        assert err.decode() == expected_stderr
+    except AssertionError:
+        pathlib.Path(os.path.join(source, "failure")).mkdir(exist_ok=True)
+        diff = subprocess.Popen(
+            ["diff", "-N", exp_err_file, "-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        diff_out, diff_err = diff.communicate(input=err)
+        with open(os.path.join(source, "failure/stderr"), "w") as fail_out:
+            fail_out.write(diff_out.decode())
+        raise
