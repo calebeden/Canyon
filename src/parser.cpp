17ecea8b5da537726732d24b0d9fa334f2a08a1e
Caleb Eden <58373773+calebeden@users.noreply.github.com>
2024-12-19 23:01:34 -0600

    fix(Parser): do not require semicolon following while in order to become a statement

--- src/parser.cpp
+++ src/parser.cpp
@@ -234,8 +234,6 @@ std::unique_ptr<BlockExpression> Parser::parseBlock() {
 			continue;
 		}
 		auto *p3 = dynamic_cast<Punctuation *>(tokens[i].get());
-		auto *block = dynamic_cast<BlockExpression *>(expr.get());
-		auto *ifelse = dynamic_cast<IfElseExpression *>(expr.get());
 		if (p3 != nullptr && p3->type == Punctuation::Type::Semicolon) {
 			i++;
 			statements.push_back(
@@ -248,18 +246,24 @@ std::unique_ptr<BlockExpression> Parser::parseBlock() {
 			}
 			return std::make_unique<BlockExpression>(*p1, std::move(statements), nullptr,
 			      *p3);
-		} else if (block != nullptr) {
+		} else if (dynamic_cast<BlockExpression *>(expr.get()) != nullptr) {
 			// A block expression can be a statement without semicolon if not at the end
 			// of the enclosing scope
 			statements.push_back(
 			      std::make_unique<ExpressionStatement>(std::unique_ptr<BlockExpression>(
 			            dynamic_cast<BlockExpression *>(expr.release()))));
-		} else if (ifelse != nullptr) {
+		} else if (dynamic_cast<IfElseExpression *>(expr.get()) != nullptr) {
 			// An if/else expression can be a statement without semicolon if not at the
 			// end of the enclosing scope
 			statements.push_back(
 			      std::make_unique<ExpressionStatement>(std::unique_ptr<IfElseExpression>(
 			            dynamic_cast<IfElseExpression *>(expr.release()))));
+		} else if (dynamic_cast<WhileExpression *>(expr.get()) != nullptr) {
+			// A while expression can be a statement without semicolon if not at the end
+			// of the enclosing scope
+			statements.push_back(
+			      std::make_unique<ExpressionStatement>(std::unique_ptr<WhileExpression>(
+			            dynamic_cast<WhileExpression *>(expr.release()))));
 		} else {
 			errorHandler->error(*tokens[i], "Expected '}'");
 			return nullptr;
